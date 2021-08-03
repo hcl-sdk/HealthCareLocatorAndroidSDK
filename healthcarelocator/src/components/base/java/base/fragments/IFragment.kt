432009b6d0a68f1e9021d5668b9bd18c5693523d
Minh-Tuan Ho-Vu <minh-tuan.ho-vu@ekino.com>
2021-08-03 10:26:46 +0700

    HCL-SDK-71: fix result highlight in list

--- healthcarelocator/src/components/base/java/base/fragments/IFragment.kt
+++ healthcarelocator/src/components/base/java/base/fragments/IFragment.kt
@@ -1,15 +1,23 @@
 package base.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 abstract class IFragment : Fragment() {
     open var fragmentTitle = ""
+    open val locationSelection = "Pref.locationSelection"
+    open val isLocationSelection = "Pref.isLocationSelection"
+    companion object {
+        lateinit var sharedPreferences: SharedPreferences
+    }
 
     open fun onUpdateFragment() {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = context!!.getSharedPreferences("SampleOneKeySDK", Context.MODE_PRIVATE)
 //        activity?.onBackPressedDispatcher?.addCallback(activity!!, object : OnBackPressedCallback(true) {
 //            override fun handleOnBackPressed() {
 //                isEnabled = false
